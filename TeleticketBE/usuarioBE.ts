12a56f35f55b7a9e553d2d5554ed95e8880d4308
andresG1802 <andresdaniel.go2020@gmail.com>
2023-06-11 17:17:45 -0500

    Actualizando modelo usuarioBE

--- TeleticketBE/usuarioBE.ts
+++ TeleticketBE/usuarioBE.ts
@@ -5,12 +5,26 @@ const Usuario = db.define('Usuario', {
     nombre: {
         type: DataTypes.STRING
     },
+    apellido:{
+        type: DataTypes.STRING
+    },
     email: {
         type: DataTypes.STRING
     },
-    estado: {
-        type: DataTypes.BOOLEAN
+    password:{
+        type:DataTypes.STRING
     },
+    direccion:{
+        type:DataTypes.STRING
+    },
+    telefono:{
+        type:DataTypes.STRING
+    },
+    estado:{
+        type:DataTypes.BOOLEAN,
+        defaultValue:true
+    }
+        
 },
 {
     //para que corra el modelo 
